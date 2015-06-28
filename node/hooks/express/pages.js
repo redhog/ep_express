8d24afd40a4bc5b5ddae16eadfcf243c3bd094b8
Egil Moeller <egil.moller@freecode.no>
2015-06-28 10:36:18 +0200

    First stab at a socketio mapper

--- node/hooks/express/pages.js
+++ node/hooks/express/pages.js
@@ -2,7 +2,90 @@ var eejs = require("eejs")
 var hooks = require("ep_carabiner/static/js/hooks.js");
 var urlmapper = require("ep_express/node/urlmapper");
 var async = require('async');
+var plugins = require('ep_carabiner/static/js/plugins');
+var webaccess = require('ep_express/node/webaccess');
 
+function extractSocketIoRegistrationList() {
+  var registrations = [];
+  plugins.parts.forEach(function (part) {
+    if (part.iomap != undefined) {
+      for (var message in part.iomap) {
+        var handler = part.iomap[message].split(":");
+        var namespace = '/';
+        if (message.indexOf(":") != -1) {
+          message = message.split(":");
+          namespace = message[0];
+          message = message[1];
+        }
+        registrations.push({namespace: namespace, message: message, handler_mod: handler[0], handler_fn: handler[1]});
+      }
+    }
+  });
+  return registrations;
+}
+
+function loadSocketIoRegistrationList(cb) {
+  var registrations = extractSocketIoRegistrationList();
+  async.each(
+    registrations,
+    function (registration, cb) {
+      plugins.loadModule(registration.handler_mod, function (mod) {
+        registration.handler = mod[registration.handler_fn];
+        cb();
+      });
+    },
+    function () {
+      cb(registrations);
+    }
+  );
+}
+
+function loadSocketIoRegistrations(cb) {
+  loadSocketIoRegistrationList(function (registrations) {
+    var namespaces = {};
+    registrations.map(function (registration) {
+      if (namespaces[registration.namespace] == undefined) {
+        namespaces[registration.namespace] = {};
+      }
+      namespaces[registration.namespace][registration.message] = registration.handler;
+    });
+
+      console.log(["socket.io", namespaces]);
+    cb(namespaces);
+  });
+}
+
+function registerSocketIoNamespace(io, namespaces) {
+  Object.keys(namespaces).map(function (namespace) {
+    var registrations = namespaces[namespace];
+
+    var channel = io.of(namespace);
+    channel.on('connection', function (socket) {
+
+      Object.keys(registrations).map(function (message) {
+        var handler = registrations[message];
+        socket.on(message, function () {
+          var handler_args = [channel, socket].concat(Array.prototype.slice.call(arguments));
+          webaccess.authorize({req: socket.conn.request, resource: namespace + ":" + message}, function (authorized) {
+            if (!authorized) {
+              console.warn("Unauthorized socket io request: " + namespace + ":" + message);
+            } else {
+              handler.apply(this, handler_args);
+            }
+          });
+        });
+      });
+
+    });
+  });
+}
+
+exports.socketio = function (hook_name, args, cb) {
+  loadSocketIoRegistrations(function (namespaces) {
+    registerSocketIoNamespace(args.io, namespaces);
+    cb();
+  });
+}
 
 exports.render = function (req, res, next) {
   async.eachSeries(urlmapper.lookup(req.url), function (match, cb) {
