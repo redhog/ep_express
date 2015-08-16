f0984ee37e312ba7778e5a7792939f9ff498c5f0
Egil Moeller <egil.moller@freecode.no>
2015-08-16 11:30:20 +0200

    jQuery doesn't really like dojo

--- static/js/rjquery.js
+++ static/js/rjquery.js
@@ -1,7 +1,7 @@
 /* Proviedes a require'able version of jQuery without leaking $ and jQuery;
  * works around wierdnesses in jquerys own packaging that makes dummy be undefined in the call below
  */
-define.amd.jQuery = true;
+// This only works with requirejs... define.amd.jQuery = true;
 define(["ep_express/static/js/jquery"], function (dummy) {
   return window.$;
   // Loading jQuery extensions won't work if you use noConflict :/
